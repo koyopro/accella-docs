ef09d344a55e3c05324fe190345e1f390664aecc
koyopro <koyopro2.0@gmail.com>
2025-01-29 03:56:00 +0900

    Use custom css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Accella',
-			social: {
-				github: 'https://github.com/koyopro/accella',
-			},
-			// sidebar: [
-			// 	{
-			// 		label: 'Guides',
-			// 		items: [
-			// 			// Each item here is one entry in the navigation menu.
-			// 			{ label: 'Example Guide', slug: 'guides/example' },
-			// 		],
-			// 	},
-			// 	{
-			// 		label: 'Reference',
-			// 		autogenerate: { directory: 'reference' },
-			// 	},
-			// ],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Accella",
+      social: {
+        github: "https://github.com/koyopro/accella",
+      },
+      customCss: ["./src/styles/custom.css"],
+      // sidebar: [
+      // 	{
+      // 		label: 'Guides',
+      // 		items: [
+      // 			// Each item here is one entry in the navigation menu.
+      // 			{ label: 'Example Guide', slug: 'guides/example' },
+      // 		],
+      // 	},
+      // 	{
+      // 		label: 'Reference',
+      // 		autogenerate: { directory: 'reference' },
+      // 	},
+      // ],
+    }),
+  ],
 });
