a35f8af1fc8300d167cd517448cd8089319c1349
koyopro <koyopro2.0@gmail.com>
2025-01-31 17:54:42 +0900

    Added Japanese language settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,18 +11,25 @@ export default defineConfig({
         github: "https://github.com/koyopro/accella",
       },
       customCss: ["./src/styles/custom.css"],
+      defaultLocale: "root",
+      locales: {
+        root: {
+          label: 'English',
+          lang: 'en',
+        },
+        'ja': {
+          label: '日本語',
+        },
+      },
       sidebar: [
-      	{
-      		label: 'Start a new project',
-      		items: [
-            'guides/installation',
-            'guides/database',
-      		],
-      	},
-      	// {
-      	// 	label: 'Reference',
-      	// 	autogenerate: { directory: 'reference' },
-      	// },
+        {
+          label: "Start a new project",
+          items: ["guides/installation", "guides/database"],
+        },
+        // {
+        // 	label: 'Reference',
+        // 	autogenerate: { directory: 'reference' },
+        // },
       ],
     }),
   ],
